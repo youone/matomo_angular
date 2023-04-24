3f691bb4dbfc0b7ed2ea56fc7c06fc4f619f330f
Johan Lundqvist <jolundq@gmail.com>
2023-04-24 11:40:51 +0200

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatomoTracker } from '@ngx-matomo/tracker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private readonly tracker: MatomoTracker) {}
   title = 'matomo';
+  ngOnInit() {
+    this.tracker.trackPageView();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgxMatomoTrackerModule } from '@ngx-matomo/tracker';
+import { NgxMatomoRouterModule } from '@ngx-matomo/router';
+
+console.log('ver: ', 2)
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxMatomoTrackerModule.forRoot({ trackerUrl: 'https://you1.matomo.cloud/matomo.php', siteId: '1', scriptUrl: 'https://you1.matomo.cloud/matomo.js' }),
+    NgxMatomoRouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
